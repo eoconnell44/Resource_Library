5dd61d803252c6beecb7a057f738cfa124eea021
Ed O'Connell <eoconnell44@gmail.com>
2017-05-03 13:51:21 -0400

    seed and db created

---
+++ devLibrary/db/queries.js
@@ -0,0 +1,28 @@
+var pgp = require('pg-promise')();
+//Port that postgres listens to
+var connString = 'postgres://localhost:5432/contacts_db1';
+var db = pgp(connString);
+
+function getAllResources(){
+
+}
+
+function getOneResource(){
+
+}
+
+function createResource(){
+
+}
+
+function updateResource() {
+
+}
+
+function deleteResource() {
+
+}
+
+// module.exports = {
+	
+// }
---
+++ devLibrary/libraryRsrcs.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS library_resources;
+CREATE DATABASE library_resources;
+
+\c library_resources;
+
+CREATE TABLE resources (
+	ID SERIAL PRIMARY KEY,
+	title VARCHAR,
+	subject VARCHAR,
+	link VARCHAR,
+	date DATE
+);
+
+INSERT INTO resources (title, subject, link)
+	VALUES('SQL DATE', 'SQL', 'https://www.w3schools.com/sql/sql_dates.asp');
+	
